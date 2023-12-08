49762830bae32c4b69ddc8c10a1993a83c0ce4db
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2023-12-08 09:32:30 -0500

    chore: set tailwind and fonts

--- pawsome-walks/astro.config.mjs
+++ pawsome-walks/astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from "astro/config";
 import { defaultLocale, locales } from "./src/i18n/language";
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   i18n: {
     defaultLocale,
     locales,
     routing: {
-      prefixDefaultLocale: true,
-    },
+      prefixDefaultLocale: true
+    }
   },
   output: "server",
+  integrations: [tailwind()]
 });
